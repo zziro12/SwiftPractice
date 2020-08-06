4646806a56e45db6d117341a0d2fe603f8a3bc43
zziro12 <opers7@naver.com>
2020-08-06 20:48:43 +0900

    finish class

--- Class_inheritance.playground/Contents.swift
+++ Class_inheritance.playground/Contents.swift
@@ -61,6 +61,14 @@ class Person {
 //상속
 class Student: Person {
     var grades: [Grade] = []
+    
+    override init(firstName: String, lastName: String) {
+        super.init(firstName: firstName, lastName: lastName)
+    }
+    
+    convenience init(student : Student){
+        self.init(firstName : student.firstName, lastName : student.lastName)
+    }
 }
 
 
@@ -89,6 +97,21 @@ jin.grades.count
 class StudentAthlete: Student {
     var minimumTrainingTime: Int = 2
     var trainedTime: Int = 0
+    var sports : [String]
+    
+    init(firstName: String, lastName: String, sports : [String]) {
+        // Phase 2
+        self.sports = sports
+        //self.train    property가 세팅도 되기전에 메소드 호출을 하면 오류가 뜬다
+        super.init(firstName: firstName, lastName: lastName)
+        
+        // Phase 2
+        self.train()
+    }
+    
+    convenience init(name: String) {
+        self.init(firstName : name, lastName : "", sports : [])
+    }
     
     func train() {
         trainedTime += 1
@@ -107,34 +130,42 @@ class FootballPlayer: StudentAthlete {
 
 // Person > Student > Athelete > Football Player
 
-var athelete1 = StudentAthlete(firstName: "Yuna", lastName: "Kim")
-var athelete2 = FootballPlayer(firstName: "Heungmin", lastName: "Son")
+//var athelete1 = StudentAthlete(firstName: "Yuna", lastName: "Kim")
+//var athelete2 = FootballPlayer(firstName: "Heungmin", lastName: "Son")
 
-athelete1.firstName
-athelete2.firstName
+//athelete1.firstName
+//athelete2.firstName
+//
+//athelete1.grades.append(math)
+//athelete2.grades.append(math)
+//
+//athelete1.minimumTrainingTime
+//athelete2.minimumTrainingTime
+//
+////athelete1.footballTeam
+//athelete2.footballTeam
+//
+//athelete1.train()
+//athelete1.trainedTime
+//
+////athelete2.train()
+////athelete2.trainedTime
+//
+//
+//
+//athelete1 = athelete2 as StudentAthlete
+//athelete1.train()
+//athelete1.trainedTime
+//
+//
+//if let son = athelete1 as? FootballPlayer {
+//    print("--> team:\(son.footballTeam)")
+//}
 
-athelete1.grades.append(math)
-athelete2.grades.append(math)
+let student1 = Student(firstName: "jinho", lastName: "Choi")
+let student11 = Student(student: student1)
+student11.firstName
+student11.lastName
+let student2 = StudentAthlete(firstName: "memmy", lastName: "Kim", sports: ["UFC"])
 
-athelete1.minimumTrainingTime
-athelete2.minimumTrainingTime
-
-//athelete1.footballTeam
-athelete2.footballTeam
-
-athelete1.train()
-athelete1.trainedTime
-
-//athelete2.train()
-//athelete2.trainedTime
-
-
-
-athelete1 = athelete2 as StudentAthlete
-athelete1.train()
-athelete1.trainedTime
-
-
-if let son = athelete1 as? FootballPlayer {
-    print("--> team:\(son.footballTeam)")
-}
+let student3 = StudentAthlete(name: "james")
