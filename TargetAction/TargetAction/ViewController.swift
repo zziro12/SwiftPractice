86f64c04223b7a96266fc3d6d8c53a77ffa2ac68
zziro12 <opers7@naver.com>
2020-08-27 17:50:18 +0900

    init TargetAction

---
+++ TargetAction/TargetAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TargetAction
+//
+//  Created by Jinho Choi on 2020/08/27.
+//  Copyright © 2020 Jinho Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
