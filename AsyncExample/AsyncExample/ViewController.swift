0401a750e2d27458f55aa49bee2e133e9f1cd664
zziro12 <opers7@naver.com>
2020-09-28 19:05:02 +0900

    so hard..

--- AsyncExample/AsyncExample/ViewController.swift
+++ AsyncExample/AsyncExample/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func touchUpDownloadButton(_ sender: UIButton) {
+        //이미지 다운로드 -> 이미지 뷰에 셋팅
+        print("button")
+        
+        // https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg
+        let imageURL: URL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg")!
+        
+        
+        OperationQueue().addOperation {
+            let imageData: Data = try! Data.init(contentsOf: imageURL)
+            let image: UIImage = UIImage(data: imageData)!
+            OperationQueue.main.addOperation {
+                self.imageView.image = image
+            }
+            
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
