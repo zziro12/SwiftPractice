6dfcac6e321ff38104ef92a7b19666ef543d9c7f
zziro12 <opers7@naver.com>
2020-10-05 02:23:10 +0900

    study readLine

---
+++ readLineEX/readLineEX/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  readLineEX
+//
+//  Created by Jinho Choi on 2020/10/04.
+//
+
+import Foundation
+
+
+//print("수를 입력해주세요")
+//let number = readLine()
+//if let number = number {
+//    print("입력된 수는 \(number) 입니다.")
+//}
+
+//gugudan()
+//while let myNumber = readLine() {
+//    print(myNumber)
+//}
+//let ss = 1
+//if (ss in -1...1) {
+//    print("다시")
+//}
